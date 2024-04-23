e799c1c8c51870e226698bb99b5ce15891c793f8
rfechner <richard.fechner.nr@gmail.com>
2024-04-23 17:14:29 +0200

    Vectorized MPPI

--- mbrl/controllers/mpc_mppi_torch.py
+++ mbrl/controllers/mpc_mppi_torch.py
@@ -53,7 +53,8 @@ class TorchMpcMPPI(MpcController):
                 raise Exception(f"Model {self.forward_model} not supported!")
 
         if self._ensemble_size:
-            self.forward_model.num_simulated_trajectories = self.num_sim_traj
+            # needed for predicting multiple trajectories in parallel
+            self.forward_model.num_simulated_trajectories = self.num_envs * self.num_sim_traj
             self.forward_model.horizon = self.horizon
             if hasattr(self.forward_model, "preallocate_memory"):
                 self.forward_model.preallocate_memory()
@@ -79,38 +80,51 @@ class TorchMpcMPPI(MpcController):
         """
 
         # nominal actions buffer
-        self.u_ = torch.zeros(self.dim_samples, device=torch_helpers.device, dtype=torch.float32)
+        self.u_ = torch.zeros(size=(self.num_envs,
+                              *self.dim_samples), device=torch_helpers.device, dtype=torch.float32)
+
         
         # standart deviation of error
-        self.std_ = torch.ones(self.dim_samples, device=torch_helpers.device, dtype=torch.float32)
+        self.std_ = torch.ones(size=(self.num_envs,
+                               *self.dim_samples), device=torch_helpers.device, dtype=torch.float32)
 
         self.action_high_tensor = torch.zeros_like(self.u_, device=torch_helpers.device, dtype=torch.float32)
         self.action_high_tensor[..., :] = torch.from_numpy(self.env.action_space.high).float().to(torch_helpers.device)
         self.action_low_tensor = torch.zeros_like(self.u_, device=torch_helpers.device, dtype=torch.float32)
         self.action_low_tensor[..., :] = torch.from_numpy(self.env.action_space.low).float().to(torch_helpers.device)
 
-        self.delta_u_ = torch.zeros(
+        self.delta_u_ = torch.zeros( size=(self.num_envs,
             self.num_sim_traj,
-            *self.u_.shape,
+            *self.dim_samples),
+            device=torch_helpers.device,
+            dtype=torch.float32,
+        )
+
+        self.start_obs_ = torch.zeros(
+            (
+                self.num_envs,
+                self.num_sim_traj,
+                self.env.observation_space.shape[0],
+            ),
             device=torch_helpers.device,
             dtype=torch.float32,
         )
 
         if self.state_dim is not None:
-            self.start_states_ = torch.empty((self.num_sim_traj, self.state_dim))
+            self.start_states_ = torch.empty((self.num_envs, self.num_sim_traj, self.state_dim))
         else:
-            self.start_states_ = [None] * self.num_sim_traj
+            self.start_states_ = [[None] * self.num_sim_traj] * self.num_envs
 
         if self._ensemble_size:
             self.costs_per_model_ = torch.zeros(
-                (self.num_sim_traj, self._ensemble_size),
+                (self.num_envs, self.num_sim_traj, self._ensemble_size),
                 device=torch_helpers.device,
                 dtype=torch.float32,
             )
-            self.costs_ = torch.zeros(self.num_sim_traj, device=torch_helpers.device, dtype=torch.float32)
-            self.costs_std_ = torch.zeros(self.num_sim_traj, device=torch_helpers.device, dtype=torch.float32)
+            self.costs_ = torch.zeros(self.num_envs, self.num_sim_traj, device=torch_helpers.device, dtype=torch.float32)
+            self.costs_std_ = torch.zeros(self.num_envs, self.num_sim_traj, device=torch_helpers.device, dtype=torch.float32)
         else:
-            self.costs_ = torch.zeros(self.num_sim_traj, device=torch_helpers.device, dtype=torch.float32)
+            self.costs_ = torch.zeros(self.num_envs, self.num_sim_traj, device=torch_helpers.device, dtype=torch.float32)
 
     @torch.no_grad()
     def set_init_action(self, action):
@@ -120,13 +134,13 @@ class TorchMpcMPPI(MpcController):
     def trajectory_cost_fn(self, cost_fn, rollout_buffer: RolloutBuffer, out: torch.Tensor):
         if self.use_env_reward:
             raise NotImplementedError()
-        else:
-            costs_path = cost_fn(
+        
+        costs_path = cost_fn(
                 rollout_buffer.as_array("observations"),
                 rollout_buffer.as_array("actions"),
                 rollout_buffer.as_array("next_observations"),
-            )  # shape: [p,h]
-
+        )  # shape: [p,h]
+            
         # Watch out: result is written to preallocated variable 'out'
         if self.cost_along_trajectory == "sum":
             return torch.sum(costs_path, axis=-1, out=out)
@@ -142,6 +156,9 @@ class TorchMpcMPPI(MpcController):
     @torch.no_grad()
     def beginning_of_rollout(self, *, observation, state=None, mode):
         super().beginning_of_rollout(observation=observation, state=state, mode=mode)
+        self.reset_mean(self.u_, self.relative_init)
+        self.reset_std(self.std_, self.relative_init)
+        self.was_reset = True
 
     @torch.no_grad()
     def end_of_rollout(self, total_time, total_return, mode):
@@ -149,19 +166,19 @@ class TorchMpcMPPI(MpcController):
 
     def sample_action_sequences(self):
     
-        # (num_trajs, horizon_n, a_dim)
+        # (num_envs, num_trajs, horizon_n, a_dim)
         torch.randn(size=self.delta_u_.shape,
                               device=torch_helpers.device, dtype=torch.float32, out=self.delta_u_)
         
         # multiply with std, write back into self.delta_u_
-        torch.mul(self.delta_u_, self.std_, out=self.delta_u_)
+        torch.mul(self.delta_u_, self.std_[:, None, ...], out=self.delta_u_)
 
         # broadcast the deltas onto the nominal actions.
-        action_sequences = self.delta_u_ + self.u_[None, ...]
+        action_sequences = self.delta_u_ + self.u_[:, None, ...]
         
         # clip for legal action range
-        torch.min(action_sequences, self.action_high_tensor, out=action_sequences)
-        torch.max(action_sequences, self.action_low_tensor, out=action_sequences)
+        torch.min(action_sequences, self.action_high_tensor[:, None, ...], out=action_sequences)
+        torch.max(action_sequences, self.action_low_tensor[:, None, ...], out=action_sequences)
 
         return action_sequences
     
@@ -172,7 +189,7 @@ class TorchMpcMPPI(MpcController):
 
             Args:
 
-                obs : current observation from the environment. Shape (o_dim,)
+                obs : current observation from the environment. Shape (num_envs, o_dim)
                 state: current state of the forward model (only used in stateful models). Shape (fwmodel_state_dim,)
                 mode: flag to switch between evaluation and training mode.
 
@@ -181,15 +198,22 @@ class TorchMpcMPPI(MpcController):
                 executed_action: Action at index 0 of the best found action sequence. Shape (a_dim,)
         
         """
+        if not self.was_reset:
+            raise AttributeError("beginning_of_rollout() needs to be called before")
+
         self.forward_model_state = self.forward_model.got_actual_observation_and_env_state(
             observation=obs, env_state=state, model_state=self.forward_model_state
         )
-
+        
         # sample action sequences v around the nominal (mean) action sequence u
         action_sequences = self.sample_action_sequences()
         
-        # repeat the obs to match num_trajs. shape: (num_trajs, o_dim)
-        obs_ = torch.atleast_2d(torch.from_numpy(obs)).repeat(self.num_sim_traj, 1).to(torch_helpers.device)
+        # repeat the obs to match num_trajs. shape: (num_envs, num_trajs, o_dim)
+        # obs has shape (num_env, o_dim) -> unsqueeze to (num_envs, 1 , o_dim), repeat
+        obs_ = torch.from_numpy(obs)\
+            .unsqueeze(1)\
+            .repeat(1, self.num_sim_traj, 1)\
+            .to(torch_helpers.device)
 
         # Monte Carlo Simulation 
         rollouts = self.simulate_trajectories(obs=obs_, 
@@ -201,36 +225,42 @@ class TorchMpcMPPI(MpcController):
         if self._ensemble_size:
                 self.trajectory_cost_fn(
                     self.cost_fn, rollouts, out=self.costs_per_model_
-                )  # shape [num_sim_traj, num_models]
+                )  # shape [num_envs, num_sim_traj, num_models]
 
-                torch.mean(self.costs_per_model_, -1, out=self.costs_)
+                # TODO: mean over which dimension? -> Should sum out the ensemble dimension.
+                torch.mean(self.costs_per_model_, -1, out=self.costs_) # shape: [num_envs, self.num_sim_traj]
                 # could be used to weigh the costs
                 torch.std(self.costs_per_model_, -1, out=self.costs_std_)
 
                 if self.use_ensemble_cost_std:
                     torch.add(self.costs_, self.costs_std_, out=self.costs_)
         else:
-            self.trajectory_cost_fn(self.cost_fn, rollouts, out=self.costs_)  # shape: [num_sim_paths]
+            # TODO: shape correct?
+            self.trajectory_cost_fn(self.cost_fn, rollouts, out=self.costs_)  # shape: [self.num_envs, self.num_sim_traj]
             
         # calculate weighting. The less cost a action sequence has accumulated, the 
         # heavier its influence on the best action sequence should be.
-        torch.subtract(self.costs_, self.costs_.min(0)[0], out=self.costs_)
-        torch.mul(self.costs_, 1/self.temperature)
+        # TODO: check all shapes from here on out.
+        # Calculate the min over the last dimension === the trajectory dimension.
+        torch.subtract(self.costs_, self.costs_.min(-1, keepdims=True)[0], out=self.costs_)
+        torch.mul(self.costs_, 1/self.temperature, out=self.costs_)
 
-        w = torch.softmax(-self.costs_, dim=0)
+        w = torch.softmax(-self.costs_, dim=-1) # shape (num_envs, self.num_sim_traj)
 
         # mult w onto self.delta_u_, then add to the nominal u. 
         # This needs to follow broadcasting rules, so we need
         # to expand w's dimensions accordingly
-        torch.add(self.u_, torch.sum(self.delta_u_ * w[:, None, None], dim=0), out=self.u_)
+        trajectory_dimension = 1
+        torch.add(self.u_, torch.sum(self.delta_u_ * w[..., None, None], dim=trajectory_dimension), out=self.u_)
 
+        # self.u_ has shape [num_envs, horizon, a_dim]
         # return best action
-        executed_action = self.u_[0]
+        executed_action = self.u_[:, 0]
         executed_action = executed_action.cpu().detach().numpy()
 
-        # shift u_ (means) leftwards
-        self.u_ = torch.concat([self.u_[1:], 
-                         torch.zeros(size=(1, self.a_dim), device=torch_helpers.device, dtype=torch.float32)], dim=0)
+        # shift u_ (means) leftwards. The right-most entry is filled with zeros
+        self.u_ = torch.concat([self.u_[:, 1:], 
+                         torch.zeros(size=(self.num_envs, 1, self.a_dim), device=torch_helpers.device, dtype=torch.float32)], dim=1)
 
         if self.mpc_hook:
             self.mpc_hook.executed_action(obs, executed_action)
@@ -266,14 +296,28 @@ class TorchMpcMPPI(MpcController):
             if state is not None:
                 self.start_states_[:] = to_tensor(state[None]).to(torch_helpers.device)
             else:
-                self.start_states_ = [None] * self.num_sim_traj
+                self.start_states_ = [None] * self.num_sim_traj * self.num_envs
 
-            return self.forward_model.predict_n_steps(
+            # TODO: why are we calling the forward model with start_obs???
+            rb = self.forward_model.predict_n_steps(
+                start_observations=obs.reshape(-1, obs.shape[-1]),
+                start_states=self.start_states_,
+                policy=OpenLoopPolicy(action_sequences.reshape(-1, action_sequences.shape[-2], action_sequences.shape[-1])),
+                horizon=self.horizon,
+            )[0]
+            for k in rb.buffer.keys():
+                if self._ensemble_size:
+                    rb.buffer[k] = rb.buffer[k].reshape(self.num_envs, self.num_sim_traj, self._ensemble_size, self.horizon, rb.buffer[k].shape[-1])
+                else:
+                    rb.buffer[k] = rb.buffer[k].reshape(self.num_envs, self.num_sim_traj, self.horizon, rb.buffer[k].shape[-1])
+            return rb
+        
+            """return self.forward_model.predict_n_steps(
                 start_observations=obs,
                 start_states=self.start_states_,
                 policy=OpenLoopPolicy(action_sequences),
                 horizon=self.horizon,
-            )[0]
+            )[0]"""
         
     def _parse_action_sampler_params(
         self,
@@ -329,3 +373,18 @@ class TorchMpcMPPI(MpcController):
             self.dim_samples = (self.horizon, self.env.action_space.shape[0])
         else:
             raise NotImplementedError
+    
+    @torch.no_grad()
+    def reset_mean(self, tensor, relative):
+        if relative:
+            torch.add(self.action_high_tensor, self.action_low_tensor, out=tensor)
+            torch.mul(tensor, 2.0, out=tensor)
+        else:
+            tensor.fill_(0)
+
+    def reset_std(self, tensor, relative):
+        if relative:
+            torch.subtract(self.action_high_tensor, self.action_low_tensor, out=tensor)
+            torch.mul(tensor, self.init_std / 2.0, out=tensor)
+        else:
+            tensor.fill_(self.init_std)
